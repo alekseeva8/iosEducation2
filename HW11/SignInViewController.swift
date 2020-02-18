7b716aa00f97a17ee7ffcb454cd1e9964b3e54ab
alekseeva8 <alekseeva.ev8@gmail.com>
2020-02-18 16:13:17 +0300

    HW11 (TableView doesn't work properly)

--- HW11/SignInViewController.swift
+++ HW11/SignInViewController.swift
@@ -15,15 +15,25 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var signButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     @IBAction func signInButtonPressed(_ sender: Any) {
-        //здесь будут условия для нажатия кнопки ??
+        //условия для перехода к другому экрану (при нажатии на кнопку)
+        
+        let textOfLogIn = loginTextField.text ?? ""
+        let numbOfSymbolsInLogIn = textOfLogIn.count
+        let textOfPassword = passwordTextField.text ?? ""
+        let numbOfSymbolsInPassword = textOfPassword.count
+        
+        if numbOfSymbolsInLogIn > 0 && numbOfSymbolsInPassword >= 6 {
+                performSegue(withIdentifier: "fromSignToMain", sender: nil)
+        }
     }
-    
+    //передача значения, введенного в loginTextField, в label другого viewcontrollerа
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let mainVC = segue.destination as? MainViewController {
             let infToMainVC = loginTextField.text ?? ""
@@ -31,7 +41,4 @@ class SignInViewController: UIViewController {
         }
         
     }
-
-    
-
 }
